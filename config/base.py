176456c4b6345cd9ea4ba2a3be9af5b151333c16
Meetasha Gaur <meetasha.gaur1@delhivery.com>
2020-10-24 19:43:15 +0530

    add production config

--- config/base.py
+++ config/base.py
@@ -3,7 +3,7 @@ import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 
-PROJECT_NAME = 'wms-user-management'
+PROJECT_NAME = 'fynd_assignment'
 DEBUG = False
 TESTING = False
 SQLALCHEMY_DATABASE_URI = "postgresql://postgres:wms_user@localhost:5432/imdb"
---
+++ config/production.py
@@ -0,0 +1,7 @@
+from config.base import *
+
+
+DEVELOPMENT = True
+DEBUG = True
+SQLALCHEMY_DATABASE_URI = "postgres://ocvvvpkhgjxksk:54cb7f889d7a5ad8c9d744767ebc7736f3df40c21b97931371d14e28a3a2583c@ec2-52-0-155-79.compute-1.amazonaws.com:5432/d6autdro68rvva"
+
